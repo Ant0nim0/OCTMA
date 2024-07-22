0473af4c34dbf3d6aa6a010b334060cb89d46b19
raul <rauabc9595@gmail.com>
2024-07-22 16:41:56 -0400

    Update App.tsx and other components

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,56 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { TonConnectButton } from '@tonconnect/ui-react';
 import { useTonConnect } from './hooks/useTonConnect';
-import { useCounterContract } from './hooks/useCounterContract';
 import '@twa-dev/sdk';
-
+import { useGameState } from './hooks/useGameState';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import GameArea from './components/GameArea';
+import FriendsPage from './components/FriendsPage';
+import RewardsPage from './components/RewardsPage';
+import BoostPage from './components/BoostPage';
+import AirdropPage from './components/AirdropPage';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('JUEGO');
   const { connected } = useTonConnect();
-  const { value, address, sendIncrement } = useCounterContract();
+  const { gameState, updateGameState } = useGameState();
+
+  useEffect(() => {
+    if (connected && gameState.totalCoins === 0) {
+      // Reward for connecting wallet
+      updateGameState({ totalCoins: 10000 });
+    }
+  }, [connected]);
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'JUEGO':
+        return <GameArea gameState={gameState} updateGameState={updateGameState} />;
+      case 'FRIENDS':
+        return <FriendsPage gameState={gameState} updateGameState={updateGameState} />;
+      case 'REWARDS':
+        return <RewardsPage gameState={gameState} updateGameState={updateGameState} />;
+      case 'BOOST':
+        return <BoostPage gameState={gameState} updateGameState={updateGameState} />;
+      case 'AIRDROP':
+        return <AirdropPage gameState={gameState} updateGameState={updateGameState} />;
+      default:
+        return <GameArea gameState={gameState} updateGameState={updateGameState} />;
+    }
+  };
 
   return (
-    <div className='App'>
-      <div className='Container'>
+    <div className="App">
+      <Header gameState={gameState} />
+      <div className="wallet-connect">
         <TonConnectButton />
-
-        <div className='Card'>
-          <b>Counter Address</b>
-          <div className='Hint'>{address?.slice(0, 30) + '...'}</div>
-        </div>
-
-        <div className='Card'>
-          <b>Counter Value</b>
-          <div>{value ?? 'Loading...'}</div>
-        </div>
-
-        <a
-          className={`Button ${connected ? 'Active' : 'Disabled'}`}
-          onClick={() => {
-            sendIncrement();
-          }}
-        >
-          Increment
-        </a>
       </div>
+      {renderPage()}
+      <Footer currentPage={currentPage} setCurrentPage={setCurrentPage} />
     </div>
   );
 }
 
-export default App
+export default App;
